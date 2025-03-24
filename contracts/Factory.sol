a47be8585a31a3b8c805b8c5fd4a868fbfc50c2d
Reema Sarkar <145077817+reema-s1@users.noreply.github.com>
2025-03-24 16:26:11 +0530

    initial commit with factory contract

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,4 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
 
-contract Factory {}
+contract Factory {
+
+    uint256 public immutable fee; //state var - written to bc
+    address public owner; 
+
+
+    constructor(uint256 _fee){
+        fee = _fee; //local var - can't acess in bc
+        owner = msg.sender; 
+    }
+}
+
+
+
--- test/Factory.js
+++ test/Factory.js
@@ -1,7 +1,43 @@
+// default - const Factory = require("@/ignition/modules/Factory")
 const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
+const path = require('path');
+const Factory = require(path.resolve(__dirname, '../ignition/modules/Factory'));
+
+
+
 describe("Factory", function () {
 
+    const FEE = ethers.parseUnits("0.01", 18)
+
+    async function deployFactoryFixture() {
+
+        //fetch accounts
+        const [deployer] = await ethers.getSigners();
+    
+        //FETCH THE CONTRACT
+        const Factory = await ethers.getContractFactory("Factory");
+
+        //DEPLOY THE CONTRACT
+        const factory = await Factory.deploy(FEE); //runs our constructor
+
+        return { factory , deployer }
+    }
+
+    describe("Deployment", function() {
+        
+        it("should set the fee" , async function() {
+
+            const { factory } = await loadFixture(deployFactoryFixture)
+            expect(await factory.fee()).to.equal(FEE)
+        })
+
+        it("should set the owner" , async function() {
+
+            const { factory, deployer } = await loadFixture(deployFactoryFixture)
+            expect(await factory.owner()).to.equal(deployer.address)
+        })
+    })
 })
